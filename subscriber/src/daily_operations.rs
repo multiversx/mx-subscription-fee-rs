1c2ca5e99b1ad136a4f5a4ed64412e241ba60169
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2023-09-14 11:08:35 +0300

    move to separate function

--- subscriber/src/daily_operations.rs
+++ subscriber/src/daily_operations.rs
@@ -1,12 +1,13 @@
 use core::borrow::Borrow;
 
 use auto_farm::common::address_to_id_mapper::AddressId;
-use multiversx_sc_modules::ongoing_operation::{CONTINUE_OP, STOP_OP};
+use multiversx_sc::api::StorageMapperApi;
+use multiversx_sc_modules::ongoing_operation::{LoopOp, CONTINUE_OP, STOP_OP};
 use subscription_fee::subtract_payments::{MyVeryOwnScResult, ProxyTrait as _};
 
 use crate::{
     base_functions::SubscriberContract,
-    service::{PaymentType, SubscriptionType},
+    service::{PaymentType, ServiceInfo, SubscriptionType},
 };
 
 multiversx_sc::imports!();
@@ -28,6 +29,22 @@ pub struct OperationProgress {
     pub additional_data_index: usize,
 }
 
+pub struct OperationData<
+    M: ManagedTypeApi + StorageMapperApi,
+    AdditionalDataType: ManagedVecItem + Clone,
+> {
+    pub additional_data: ManagedVec<M, AdditionalDataType>,
+    pub additional_data_len: usize,
+    pub user_index: usize,
+    pub total_users: usize,
+    pub users_mapper: UnorderedSetMapper<M, AddressId>,
+    pub service_info: ServiceInfo<M>,
+    pub service_index: usize,
+    pub current_epoch: Epoch,
+    pub fees_contract_address: ManagedAddress<M>,
+    pub energy_threshold: BigUint<M>,
+}
+
 #[multiversx_sc::module]
 pub trait DailyOperationsModule:
     crate::service::ServiceModule
@@ -41,8 +58,8 @@ pub trait DailyOperationsModule:
         user_index: &mut usize,
         additional_data: ManagedVec<SC::AdditionalDataType>,
     ) -> OperationCompletionStatus {
-        let mut users_mapper = self.subscribed_users(service_index);
-        let mut total_users = users_mapper.len();
+        let users_mapper = self.subscribed_users(service_index);
+        let total_users = users_mapper.len();
         let mut progress = self.load_operation::<OperationProgress>();
         if progress.current_index == 0 {
             progress.service_index = service_index;
@@ -55,85 +72,29 @@ pub trait DailyOperationsModule:
             );
         }
 
-        let fees_contract_address = self.fees_contract_address().get();
-        let current_epoch = self.blockchain().get_block_epoch();
-        let energy_threshold = self.energy_threshold().get();
-        let service_info = self.service_info().get().get(service_index);
-        let additional_data_len = additional_data.len();
+        let mut all_data = OperationData::<Self::Api, SC::AdditionalDataType> {
+            additional_data_len: additional_data.len(),
+            additional_data,
+            user_index: *user_index,
+            total_users,
+            users_mapper,
+            service_info: self.service_info().get().get(service_index),
+            service_index,
+            current_epoch: self.blockchain().get_block_epoch(),
+            fees_contract_address: self.fees_contract_address().get(),
+            energy_threshold: self.energy_threshold().get(),
+        };
 
         let mut output_egld = BigUint::zero();
         let mut output_esdt = ManagedVec::new();
 
         let run_result = self.run_while_it_has_gas(GAS_TO_SAVE_PROGRESS, || {
-            if progress.additional_data_index >= additional_data_len
-                || progress.current_index > total_users
-            {
-                return STOP_OP;
-            }
-
-            let user_data = additional_data.get(*user_index);
-            progress.additional_data_index += 1;
-
-            let user_id = users_mapper.get_by_index(progress.current_index);
-            let opt_user_address = self
-                .user_id()
-                .get_address_at_address(&fees_contract_address, user_id);
-            let user_address = match opt_user_address {
-                Some(address) => address,
-                None => {
-                    users_mapper.swap_remove(&user_id);
-                    total_users -= 1;
-                    return CONTINUE_OP;
-                }
-            };
-
-            progress.current_index += 1;
-
-            let subscription_type = self.subscription_type(user_id, service_index).get();
-            let next_action_epoch =
-                self.get_next_action_epoch(user_id, service_index, subscription_type);
-            if next_action_epoch > current_epoch {
-                return CONTINUE_OP;
-            }
-
-            let subtract_result = self.subtract_payment(
-                &user_address,
-                user_id,
-                &energy_threshold,
-                service_info.payment_type.clone(),
-                fees_contract_address.clone(),
-            );
-            if subtract_result.is_err() {
-                return CONTINUE_OP;
-            }
-
-            let fee = unsafe { subtract_result.unwrap_unchecked() };
-            if fee.token_identifier.is_egld() {
-                output_egld += fee.amount;
-            } else {
-                let payment =
-                    EsdtTokenPayment::new(fee.token_identifier.unwrap_esdt(), 0, fee.amount);
-                output_esdt.push(payment);
-            }
-
-            let action_results = SC::perform_action(
-                self,
-                user_address.clone(),
-                user_id,
-                &service_info,
-                user_data.borrow(),
-            );
-            if action_results.is_err() {
-                return CONTINUE_OP;
-            }
-
-            let interpreted_results = unsafe { action_results.unwrap_unchecked() };
-            self.send_user_rewards(&user_address, interpreted_results.user_rewards);
-
-            self.last_action_epoch(user_id, service_index)
-                .set(current_epoch);
-
-            CONTINUE_OP
+            self.perform_one_operation::<SC>(
+                &mut progress,
+                &mut all_data,
+                &mut output_egld,
+                &mut output_esdt,
+            )
         });
 
         if run_result == OperationCompletionStatus::InterruptedBeforeOutOfGas {
@@ -151,6 +112,85 @@ pub trait DailyOperationsModule:
         }
 
         run_result
+    }
+
+    fn perform_one_operation<SC: SubscriberContract<SubSc = Self>>(
+        &self,
+        progress: &mut OperationProgress,
+        all_data: &mut OperationData<Self::Api, SC::AdditionalDataType>,
+        output_egld: &mut BigUint,
+        output_esdt: &mut ManagedVec<EsdtTokenPayment>,
+    ) -> LoopOp {
+        if progress.additional_data_index >= all_data.additional_data_len
+            || progress.current_index > all_data.total_users
+        {
+            return STOP_OP;
+        }
+
+        let user_data = all_data.additional_data.get(all_data.user_index);
+        progress.additional_data_index += 1;
+
+        let user_id = all_data.users_mapper.get_by_index(progress.current_index);
+        let opt_user_address = self
+            .user_id()
+            .get_address_at_address(&all_data.fees_contract_address, user_id);
+        let user_address = match opt_user_address {
+            Some(address) => address,
+            None => {
+                all_data.users_mapper.swap_remove(&user_id);
+                all_data.total_users -= 1;
+                return CONTINUE_OP;
+            }
+        };
+
+        progress.current_index += 1;
+
+        let subscription_type = self
+            .subscription_type(user_id, all_data.service_index)
+            .get();
+        let next_action_epoch =
+            self.get_next_action_epoch(user_id, all_data.service_index, subscription_type);
+        if next_action_epoch > all_data.current_epoch {
+            return CONTINUE_OP;
+        }
+
+        let subtract_result = self.subtract_payment(
+            &user_address,
+            user_id,
+            &all_data.energy_threshold,
+            all_data.service_info.payment_type.clone(),
+            all_data.fees_contract_address.clone(),
+        );
+        if subtract_result.is_err() {
+            return CONTINUE_OP;
+        }
+
+        let fee = unsafe { subtract_result.unwrap_unchecked() };
+        if fee.token_identifier.is_egld() {
+            *output_egld += fee.amount;
+        } else {
+            let payment = EsdtTokenPayment::new(fee.token_identifier.unwrap_esdt(), 0, fee.amount);
+            output_esdt.push(payment);
+        }
+
+        let action_results = SC::perform_action(
+            self,
+            user_address.clone(),
+            user_id,
+            &all_data.service_info,
+            user_data.borrow(),
+        );
+        if action_results.is_err() {
+            return CONTINUE_OP;
+        }
+
+        let interpreted_results = unsafe { action_results.unwrap_unchecked() };
+        self.send_user_rewards(&user_address, interpreted_results.user_rewards);
+
+        self.last_action_epoch(user_id, all_data.service_index)
+            .set(all_data.current_epoch);
+
+        CONTINUE_OP
     }
 
     fn subtract_payment(
